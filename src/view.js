351bcd849bc1c5c8363b3afed9e95e437f3e783c
florian <vlo@guez.li>
2012-03-31 16:53:35 +0200

    Die perfekte Welle

--- src/view.js
+++ src/view.js
@@ -3,6 +3,9 @@ cells = Array();
 
 running = false;
 
+ROWS = 100;
+COLS = 100;
+
 function load() {
 	
 	var body = document.body;
@@ -11,13 +14,13 @@ function load() {
 	table.style.backgroundColor = "black";
 	body.appendChild(table);
 	
-	for (i = 0; i < 50; i++) {
+	for (i = 0; i < ROWS; i++) {
 		
 		model[i] = Array();
 		
 		var row = document.createElement("tr");
 		table.appendChild(row);
-		for (j = 0; j < 50; j++) {
+		for (j = 0; j < COLS; j++) {
 			var cell = document.createElement("td");
 			row.appendChild(cell);
 			
@@ -25,14 +28,15 @@ function load() {
 			cells[cell] = cellData;
 			model[i][j] = cellData;
 			model[i][j].cell = cell;
-			model[i][j].currentGradients[0] = 255; //i == 0 && j == 0 ? 255 : 0;
+			model[i][j].currentGradients[0] = 0; //j < COLS / 3 ? -1 : j < 2 * COLS / 3 ? 0 : 1; //i == 0 && j == 0 ? 255 : 0;
+			model[i][j].currentGradients[1] = 0;
 			
 			updateCellView(cellData);
 			
-			cell.onmouseover = (function(ii, jj) {
+			cell.onmousemove = (function(ii, jj) {
 				return function() {
 					var c = model[ii][jj];
-					c.currentGradients[0] = 0;
+					c.currentGradients[0] = 1;
 					updateCellView(c);
 				}
 			})(i, j);
@@ -48,28 +52,34 @@ function step() {
 		return;
 	}
 	
-	for (i = 0; i < 50; i++) {
-		for (j = 0; j < 50; j++) {
+	for (i = 0; i < ROWS; i++) {
+		for (j = 0; j < COLS; j++) {
 			var nord = i == 0 ? null : model[i-1][j];
-			var south = i == 49 ? null : model[i+1][j];
+			var south = i == ROWS-1 ? null : model[i+1][j];
 			var west = j == 0 ? null : model[i][j-1];
-			var east = j == 49 ? null : model[i][j+1];
+			var east = j == COLS-1 ? null : model[i][j+1];
+			
+			var x = new Dimension();
+			var y = new Dimension();
+			
 			var me = model[i][j];
-			var du = (
-				  (nord != null ? nord.currentGradients[0] - me.currentGradients[0] : 0)
-				+ (south != null ? south.currentGradients[0] - me.currentGradients[0] : 0)
-				+ (west != null ? west.currentGradients[0] - me.currentGradients[0] : 0)
-				+ (east != null ? east.currentGradients[0] - me.currentGradients[0] : 0)
-				) * 0.3 * 0.25;
-			me.nextGradients[0] = me.currentGradients[0] + du;
+			
+			x.previous = (west != null ? west : me).currentGradients;
+			x.next = (east != null ? east : me).currentGradients;
+			y.previous = (nord != null ? nord : me).currentGradients;
+			y.next = (south != null ? south : me).currentGradients;
+			
+			var du = w(me.currentGradients, [x,y]);
+			for (g = 0; g < du.length; g++) {
+				me.nextGradients[g] = me.currentGradients[g] + du[g];
+			}
 		}
 	}
 	
-	for (i = 0; i < 50; i++) {
-		for (j = 0; j < 50; j++) {
+	for (i = 0; i < ROWS; i++) {
+		for (j = 0; j < COLS; j++) {
 			var me = model[i][j];
-			var prev = me.currentGradients[0];
-			me.currentGradients[0] = me.nextGradients[0];
+			me.currentGradients = me.nextGradients;
 			updateCellView(me);
 		}
 	}
@@ -78,21 +88,60 @@ function step() {
 	
 }
 
+function w_Diffusion(me, dimensions) {
+	var du = Array();
+	du[0] = 0;
+	for(n = 0; n < dimensions.length; n++) {
+		du[0] += (dimensions[n].previous[0] - me[0]) * 0.3 / dimensions.length / 2;
+		du[0] += (dimensions[n].next[0] - me[0]) * 0.3 / dimensions.length / 2;
+	}
+	return du;
+}
+
+function w(me, dimensions) {
+	var du = Array();
+	du[0] = 0;
+	du[1] = 0;
+	for (n = 0; n < dimensions.length; n++) {
+		du[0] += (dimensions[n].previous[1] - dimensions[n].next[1]) / dimensions.length;
+		du[1] += (dimensions[n].previous[0] - me[0]) / dimensions.length / 2;
+		du[1] += (me[0] - dimensions[n].next[0]) / dimensions.length / 2;
+	}
+	return du;
+}
+
+
 function toggle() {
 	running = !running;
 	step();
 }
 
 function updateCellView(cell) {
-	var color  = Math.floor(cell.currentGradients[0]);
-	cell.cell.style.backgroundColor = "rgb(" + color + ", " + color + ", " + color + ")";
-	cell.cell.title  = cell.currentGradients[0];
+	var du = cell.currentGradients;
+	cell.cell.style.backgroundColor = getColor(du[0]);
+	//cell.cell.title = "";
+	//for (g = 0; g < du.length; g++) {
+	//	cell.cell.title += g + "=" + du[g] + ";";
+	//}
+}
+
+function getColor(du) {
+	var r = Math.min(255, Math.max(0, Math.floor(128 * (1+du))));
+	var g = Math.min(255, Math.max(0, Math.floor(128 * (1+du))));
+	var b = Math.min(255, Math.max(0, Math.floor(255 * (1+du))));
+	
+	return "rgb(" + r + "," + g + "," + b + ")";
 }
 
 function CellData() {
 	this.currentGradients = Array();
 	this.nextGradients = Array();
 	this.cell = null;
+}
+
+function Dimension() {
+	this.previous = Array();
+	this.next = Array();
 }
 
 window.onload = load;
