813f56126c8b023b17d627914cda1e66c1ff1d14
florian <vlo@guez.li>
2012-04-29 12:27:32 +0200

    Detlevs changes

--- src/view.js
+++ src/view.js
@@ -3,16 +3,24 @@ model = Array();
 
 running = false;
 
-ROWS = 100;
-COLS = 100;
+ROWS = 50;
+COLS = 50;
+SLICES= 10;
 
-WIDTH = 800;
-HEIGHT = 800;
+WIDTH = 500;
+HEIGHT = 500;
 
 iterationLabel = null;
 canvas = Array();
 ctx = Array();
 iterations = 0;
+
+
+
+supportedFunctions= {INIT : 0, DIFFUS : 1, WAFE : 2 } ;
+currentFunction= supportedFunctions.WAFE;
+	
+
 
 function load() {
 	
@@ -29,27 +37,49 @@ function load() {
 	ctx[0] = canvas[0].getContext("2d");
 	
 	canvas[1] = document.getElementById("pool1");
-	canvas[1].width = WIDTH-1;
-	canvas[1].height = HEIGHT-1;
+	canvas[1].width = WIDTH;
+	canvas[1].height = HEIGHT;
 	ctx[1] = canvas[1].getContext("2d");
 
 	canvas[2] = document.getElementById("pool2");
-	canvas[2].width = WIDTH-2;
-	canvas[2].height = HEIGHT-2;
+	canvas[2].width = WIDTH;
+	canvas[2].height = HEIGHT;
 	ctx[2] = canvas[2].getContext("2d");
 
+	cellDefaultValue = document.getElementById("SetValue");
+	
+	statusLabel = document.getElementById("statusLabel");
+	initDataTextbox = document.getElementById("InitData");
+	
 	canvas[0].onmousemove = function(e) {
-		//if (e.button > 0) {
-			var off = offset(e.target);
-			var x = Math.floor((e.clientX - off.x - 2) / (WIDTH / COLS));
-			var y = Math.floor((e.clientY - off.y - 2) / (HEIGHT / ROWS));
-			var cell = model[y][x];
-			cell.currentGradients[0] = -0.8;
+		if (e.altKey) {		
+		    var initVal= cellDefaultValue.value == null ? 1 : parseFloat(cellDefaultValue.value);
+			var cell= getCell(e);
+			cell.currentGradients[0] = initVal;
 			cell.currentGradients[1] = 0;
 			cell.currentGradients[2] = 0;
+			cell.currentGradients[3] = 0;
+			cell.currentGradients[4] = 0;
+			cell.currentGradients[5] = 0;
 			updateCellView(cell);
-		//}
+		}
+		if (e.shiftKey ) {
+			var cell = getCell(e);
+			statusLabel.innerHTML =  getCellInfo(cell); 
+		}
 	};
+		
+	canvas[1].onmousemove = function(e) {
+		if (e.shiftKey ) {
+			statusLabel.innerHTML = getCellInfo(getCell(e));									  
+		}
+	};		
+	canvas[2].onmousemove = function(e) {
+		if (e.shiftKey ) {
+			statusLabel.innerHTML = getCellInfo(getCell(e));									  
+		}
+	};		
+		
 		
 	for (var i = 0; i < ROWS; i++) {
 		
@@ -87,9 +117,14 @@ function load() {
 		}
 	}
 	
-	//model[100][100].currentGradients[0] = 1;
-	updateCellView(model[10][10]);
+	// model[10][10].currentGradients[0] = 1;
+	// updateCellView(model[10][10]);
 	
+	if( 1>2) {
+	setCells("0,0,0;10,10,1");
+	} else if(1<2){
+		setCells("0,0,0;24,24,1,1;25,24,1,1");
+	}
 	//for (i = 0; )
 	
 	/*
@@ -117,13 +152,65 @@ function offset(target) {
 	return off;
 }
 
+function getCell(e) {
+	var off = offset(e.target);
+	var x = Math.floor((e.clientX - off.x - 2) / (WIDTH / COLS));
+	var y = Math.floor((e.clientY - off.y - 2) / (HEIGHT / ROWS));
+	var cell = model[y][x];
+	return cell;
+}	
+
+function getCellInfo(cell){
+	var info= 
+		"u="+formatNum(cell.currentGradients[0]) + "</br>"+
+		"u'="+formatNum(cell.currentGradients[2]) + "</br>"+
+		"u''="+formatNum(cell.currentGradients[3])+"</br>"+
+		"u.="+formatNum(cell.currentGradients[1]) + "</br>"+
+		"u..="+formatNum(cell.currentGradients[4]);
+	return info;
+}
+
+function formatNum(num){
+  return num.toPrecision(4);
+}
+  
+  
+function initCells(){
+	var initData=initDataTextbox.value;	
+	setCells(initData);
+}	
+function setCells(data)	{
+	var idRows=data.split(";");	
+	var idx=0;
+	var maxRows=(idRows.length < ROWS? idRows.length: ROWS);
+	for (var i = 0; i < maxRows; i++) {
+		var idCols=idRows[i].split(",");
+		var y=parseFloat(idCols[0]);
+		var startX=parseFloat(idCols[1]);
+		var maxCols=(idCols.length < COLS? idCols.length: COLS);
+		for (var j = 2; j < maxCols; j++) {	
+                idx= startX+j-2;
+                idx=(idx>COLS?COLS:idx);				
+				var cellData= model[y][idx] ;
+				cellData.currentGradients[0] = parseFloat(idCols[j]); 
+				cellData.currentGradients[1] = 0;
+				cellData.currentGradients[2] = 0;
+				cellData.currentGradients[3] = 0;
+				cellData.currentGradients[4] = 0;
+				cellData.currentGradients[5] = 0;
+				updateCellView(cellData);
+		}
+	}
+}
+		
+
 function step() {
 	
 	if (!running) {
 		return;
 	}
 	
-	for (var t = 0; t < 10; t++) {
+	for (var t = 0; t < SLICES; t++) {
 
 		for (var i = 0; i < ROWS; i++) {
 			for (var j = 0; j < COLS; j++) {
@@ -142,7 +229,7 @@ function step() {
 				y.previous = (nord != null ? nord : me).currentGradients;
 				y.next = (south != null ? south : me).currentGradients;
 				
-				var du = w(me.currentGradients, [x,y]);
+				var du = calcCell(me.currentGradients, [x,y]);
 				for (var g = 0; g < du.length; g++) {
 					me.nextGradients[g] = du[g];
 				}
@@ -184,19 +271,35 @@ function w_jh(me, dimensions) {
 	return du;
 }
 
-c = 1.0;
-dt = 0.1;
+
+function calcCell(me, dimensions) {
+
+	switch(currentFunction){
+		case supportedFunctions.DIFFUS:
+			break ;
+		case supportedFunctions.WAFE:
+			return w(me, dimensions);
+		default:
+			alert("sorry not supported");
+			break;
+	}
+
+}
 
 function w(me, dimensions) {
 
+	var c = 1.0;
+	var dt = 1/SLICES;
+
 	var uCurrent = me;
 	var u = uCurrent[0];
+	
 	var udx = u - (dimensions[0].previous[0] + dimensions[1].previous[0]) / 2;
 	var udxdx = -2 * u + (dimensions[0].previous[0] + dimensions[1].previous[0]) / 2 + (dimensions[0].next[0] + dimensions[1].next[0]) / 2;
 	
 	var upp = udxdx * c;
 	var up = uCurrent[1] + upp * dt;
-	var ut = uCurrent[0] + up * dt;
+	var ut = u + up * dt;
 	
 	return [
 		ut,
@@ -206,20 +309,6 @@ function w(me, dimensions) {
 		upp,
 		ut
 	];
-
-/*
-	var y = Array();
-	y[0] = 0;
-	//y[1] = 0;
-	for (var nn = 0; nn < dimensions.length; nn++) {
-		//y[0] += ( me[0] - dimensions[nn].previous[0]) / dimensions.length;
-		y[0] += (dimensions[nn].previous[0] - 2 * me[0] + dimensions[nn].next[0]) / dimensions.length;
-	}
-
-	for (var ss = 101; ss < (1.0 / dt); ss++) {
-		var k = waerme(y);
-		y = add(y, multiply(dt, k));
-	}*/
 	
 	return y;
 }
@@ -296,23 +385,21 @@ function toggle() {
 function updateCellView(cell) {
 
 	var du = cell.currentGradients;
-	var c = getColor(du[0]);
 	
-	ctx[0].fillStyle = c;
-	
-	var width = WIDTH / COLS;
+	var width = WIDTH / COLS;  		// TODO: calc this var just once
 	var height = HEIGHT / ROWS;
 	
 	var x = cell.x * width;
 	var y = cell.y * height;
 	
+	ctx[0].fillStyle = getColor(du[0]);
 	ctx[0].fillRect(x,y,width,height);
-		
-	//cell.cell.style.backgroundColor = getColor(du[0]);
-	//cell.cell.title = "";
-	//for (g = 0; g < du.length; g++) {
-	//	cell.cell.title += g + "=" + du[g] + ";";
-	//}
+
+	ctx[1].fillStyle = getColor(du[1]);
+	ctx[1].fillRect(x,y,width,height);
+
+	ctx[2].fillStyle = getColor(du[4]);
+	ctx[2].fillRect(x,y,width,height);
 }
 
 function getColor(du) {
@@ -335,5 +422,17 @@ function Dimension() {
 	this.previous = Array();
 	this.next = Array();
 }
+
+
+function CellValues() {
+	this.u=0;
+	this.up=0;
+	this.upp=0;
+	this.udx=0;
+	this.udxdx=0;
+	this.newU=0;
+}
+
+
 
 window.onload = load;
