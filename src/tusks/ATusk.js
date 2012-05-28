af1063a27a8ee4b0d45d5d15292371d4091552ec
Detlev Ziereisen <zieredet@zhaw.ch>
2012-05-28 18:00:10 +0200

    kosmetik

--- src/tusks/ATusk.js
+++ src/tusks/ATusk.js
@@ -5,16 +5,12 @@ if (typeof(module) != "undefined") {
 function ATusk() {
 
 	var tab="\t";
-
 	this.slices = 5;
-	
 	this.sayHello = function () { return "implement me!"; }
 		
 	// Berechnet den neuen Wert einer Zelle.	
 	this.calcCell = function(me, dt) {} // => du = float[] wobei du[0]=u
-	
 	this.viscosity = 0.5;
-	
 	this.damping = 0.995;
 	
 }
