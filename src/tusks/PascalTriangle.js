24ad1d376ab2017730445058ac9e5c11aa7672c7
Detlev Ziereisen <zieredet@zhaw.ch>
2012-10-24 03:26:52 +0200

    Pascal Triangle added upd

--- src/tusks/PascalTriangle.js
+++ src/tusks/PascalTriangle.js
@@ -41,10 +41,7 @@ function PascalTriangle() {
 	this.createLivingCell = function() {
 		return {status: 1};
 	}
-	this.cellStatus = function(val) {
-		return {status: val};
-	}
-	
+
 	this.createCellData = this.createDeadCell;
 
 	this.getNeighbours = new MooreNeighbourhood().getNeighbours;
@@ -54,39 +51,11 @@ function PascalTriangle() {
 	this.applyDifferentials = function(cell, dt) 	{
 		/*
 			Regeln: 
-				Quelle: http://alphard.ethz.ch/Hafner/PPS/PPS2001/Life/Life2.htm#Game
-				
-				Die Zelle ueberlebt wenn: die Zelle 2 oder 3 Nachbarn hat
-				hat eine Zelle genau 3 lebende Nachbarn so wird sie geboren
-				ansonsten stirbt sie.
+				...
 		*/
 				
-		  var val = (cell.neighbours[0].currentData.status + cell.neighbours[3].currentData.status)   ;
-		//this.setCellValue(cell, val);
+		var val = (cell.neighbours[0].currentData.status + cell.neighbours[3].currentData.status)   ;
 		return {status: Math.max(val, cell.currentData.status)};
-		
-		
-		var livingNeighours = 0;
-		for (var idx = 0; idx < cell.neighbours.length; idx++) {
-			livingNeighours += cell.neighbours[idx].currentData.status;
-		}
-
-		
-		
-		if (cell.currentData.status == 1) {
-			if (livingNeighours == 3) {
-				return this.createLivingCell();
-			}
-			if (livingNeighours == 2) {
-				return this.createLivingCell();
-			}
-		}
-		if (cell.currentData.status == 0) {
-			if (livingNeighours == 3) {
-				return this.createLivingCell();
-			}
-		}
-		return this.createDeadCell();
 		
 	}
 }
